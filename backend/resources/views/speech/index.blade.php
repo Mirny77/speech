3c7ffa2008d20eb3fad15ab5a16e44e5093a61d6
Илья <mironov.ilya20160@yandex.ru>
2021-10-22 17:11:48 +0300

    title

--- backend/resources/views/speech/index.blade.php
+++ backend/resources/views/speech/index.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Speech</title>
+        <title>Text2Speech</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
